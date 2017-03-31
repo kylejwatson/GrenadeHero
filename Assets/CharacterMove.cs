f0ab81dc9b79a176d58d6b7127325f2786e9edef
Kyle Watson <Kyle Watson>
2017-03-31 01:36:42 +0100

    movement working with physics still broken

--- Assets/CharacterMove.cs
+++ Assets/CharacterMove.cs
@@ -20,7 +20,7 @@ public class CharacterMove : MonoBehaviour {
 
 	// Update is called once per frame
 	void LateUpdate () {
-		Vector3 vel = this.GetComponent<Rigidbody> ().velocity;
+		/*Vector3 vel = this.GetComponent<Rigidbody> ().velocity;
 		vel = new Vector3 (vel.x, 0, vel.z);
 		float sprint = 1.0f;
 		if (CrossPlatformInputManager.GetButton ("Sprint")) {
@@ -31,26 +31,42 @@ public class CharacterMove : MonoBehaviour {
 		float v = CrossPlatformInputManager.GetAxisRaw ("Vertical") * totalSpeed;
 		Vector3 desiredMovement = (this.transform.forward * v) + (this.transform.right * h);
 
-		/*if (Mathf.Abs(desiredMovement.x) > Mathf.Abs(vel.x)) {
-			vel.x = desiredMovement.x;
-		}
-		if (Mathf.Abs(desiredMovement.z) > Mathf.Abs(vel.z)) {
-			vel.z = desiredMovement.z;
-		}*/
-		if (vel.sqrMagnitude < desiredMovement.sqrMagnitude) {
+		if (vel.sqrMagnitude < desiredMovement.sqrMagnitude && h+v != 0) {
 			vel = Vector3.Lerp (vel, desiredMovement, lerpSpeed);
 		} else {
 			//vel = desiredMovement - vel;
 		}
 		vel += Vector3.up * this.GetComponent<Rigidbody> ().velocity.y; 
 		this.GetComponent<Rigidbody> ().velocity = vel;
+	*/
+		float sprint = 1.0f;
+		if (CrossPlatformInputManager.GetButton ("Sprint")) {
+			sprint = sprintMultiply;
+		}
+		float totalSpeed = speed * sprint;
+		float h = CrossPlatformInputManager.GetAxisRaw ("Horizontal");
+		float v = CrossPlatformInputManager.GetAxisRaw ("Vertical");
+		Vector3 curVel = this.GetComponent<Rigidbody> ().velocity;
+		Vector3 desiredMovement = (this.transform.forward * v) + (this.transform.right * h);
+		if (curVel.x < totalSpeed && curVel.x > -totalSpeed) {
+			this.GetComponent<Rigidbody> ().velocity = new Vector3 (curVel.x + desiredMovement.x, curVel.y, curVel.z);
+		}
+		curVel = this.GetComponent<Rigidbody> ().velocity;
+		if (curVel.z < totalSpeed && curVel.z > -totalSpeed) {
+			this.GetComponent<Rigidbody> ().velocity = new Vector3 (curVel.x, curVel.y, desiredMovement.z + curVel.z);
+		}
+		//fix not actually doing term vel but doing it for each bit, learn vectors or something
+		//if (this.GetComponent<Rigidbody> ().velocity.sqrMagnitude < totalSpeed) {
+		//	this.GetComponent<Rigidbody> ().velocity += (v * this.transform.forward + h * this.transform.right);
+		//}
 
 		if (isGrounded () && CrossPlatformInputManager.GetButtonDown ("Jump")) {
 			this.GetComponent<Rigidbody> ().AddForce (Vector3.up * jumpSpeed);
 		}
+
 	}
 
 	bool isGrounded(){
-		return feet.GetComponent<CharacterFeet> ().grounded;
+		return true;//feet.GetComponent<CharacterFeet> ().grounded;
 	}
 }
