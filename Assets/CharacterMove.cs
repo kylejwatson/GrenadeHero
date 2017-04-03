7ecebb1a8f9d58d9ab51d853a185c4d4983b3d10
Kyle Watson <kylewatson98@gmail.com>
2017-04-03 23:15:58 +0100

    converted to 2.5d for easier maths

--- Assets/CharacterMove.cs
+++ Assets/CharacterMove.cs
@@ -12,7 +12,7 @@ public class CharacterMove : MonoBehaviour {
 	[SerializeField]
 	public float jumpSpeed;
 	[SerializeField]
-	public float lerpSpeed;
+	public float fakeDrag;
 	// Use this for initialization
 	void Start () {
 		
@@ -20,25 +20,6 @@ public class CharacterMove : MonoBehaviour {
 
 	// Update is called once per frame
 	void LateUpdate () {
-		/*Vector3 vel = this.GetComponent<Rigidbody> ().velocity;
-		vel = new Vector3 (vel.x, 0, vel.z);
-		float sprint = 1.0f;
-		if (CrossPlatformInputManager.GetButton ("Sprint")) {
-			sprint = sprintMultiply;
-		}
-        float totalSpeed = speed * sprint;
-		float h = CrossPlatformInputManager.GetAxisRaw ("Horizontal") * totalSpeed;
-		float v = CrossPlatformInputManager.GetAxisRaw ("Vertical") * totalSpeed;
-		Vector3 desiredMovement = (this.transform.forward * v) + (this.transform.right * h);
-
-		if (vel.sqrMagnitude < desiredMovement.sqrMagnitude && h+v != 0) {
-			vel = Vector3.Lerp (vel, desiredMovement, lerpSpeed);
-		} else {
-			//vel = desiredMovement - vel;
-		}
-		vel += Vector3.up * this.GetComponent<Rigidbody> ().velocity.y; 
-		this.GetComponent<Rigidbody> ().velocity = vel;
-	*/
 		float sprint = 1.0f;
 		if (CrossPlatformInputManager.GetButton ("Sprint")) {
 			sprint = sprintMultiply;
@@ -46,27 +27,25 @@ public class CharacterMove : MonoBehaviour {
 		float totalSpeed = speed * sprint;
 		float h = CrossPlatformInputManager.GetAxisRaw ("Horizontal");
 		float v = CrossPlatformInputManager.GetAxisRaw ("Vertical");
-		Vector3 curVel = this.GetComponent<Rigidbody> ().velocity;
-		Vector3 desiredMovement = (this.transform.forward * v) + (this.transform.right * h);
-		if (curVel.x < totalSpeed && curVel.x > -totalSpeed) {
-			this.GetComponent<Rigidbody> ().velocity = new Vector3 (curVel.x + desiredMovement.x, curVel.y, curVel.z);
+		Vector3 vel = this.GetComponent<Rigidbody> ().velocity;
+		if (vel.x > 0 && vel.x - fakeDrag >= 0) {
+			this.GetComponent<Rigidbody> ().velocity = new Vector3 (vel.x - fakeDrag, vel.y, 0);
+		}else if (vel.x < 0 && vel.x + fakeDrag <= 0) {
+			this.GetComponent<Rigidbody> ().velocity = new Vector3 (vel.x + fakeDrag, vel.y, 0);
 		}
-		curVel = this.GetComponent<Rigidbody> ().velocity;
-		if (curVel.z < totalSpeed && curVel.z > -totalSpeed) {
-			this.GetComponent<Rigidbody> ().velocity = new Vector3 (curVel.x, curVel.y, desiredMovement.z + curVel.z);
+		if (h > 0 && vel.x < totalSpeed) {
+			this.GetComponent<Rigidbody> ().velocity = new Vector3 (vel.x + h * totalSpeed/10, vel.y, 0);
+		}else if (h < 0 && vel.x > -totalSpeed) {
+			this.GetComponent<Rigidbody> ().velocity = new Vector3 (vel.x + h * totalSpeed/10, vel.y, 0);
 		}
-		//fix not actually doing term vel but doing it for each bit, learn vectors or something
-		//if (this.GetComponent<Rigidbody> ().velocity.sqrMagnitude < totalSpeed) {
-		//	this.GetComponent<Rigidbody> ().velocity += (v * this.transform.forward + h * this.transform.right);
-		//}
-
-		if (isGrounded () && CrossPlatformInputManager.GetButtonDown ("Jump")) {
+		if (isGrounded () && v == 1) {
 			this.GetComponent<Rigidbody> ().AddForce (Vector3.up * jumpSpeed);
 		}
-
+		Debug.Log ("Before : " + vel);
+		Debug.Log ("After : " + this.GetComponent<Rigidbody> ().velocity);
 	}
 
 	bool isGrounded(){
-		return true;//feet.GetComponent<CharacterFeet> ().grounded;
+		return feet.GetComponent<CharacterFeet> ().grounded;
 	}
 }
--- Assets/GrenadeThrow.cs
+++ Assets/GrenadeThrow.cs
@@ -9,28 +9,36 @@ public class GrenadeThrow : MonoBehaviour {
 	float throwStrength;
 	[SerializeField]
 	GameObject cam;
+	float dir;
 	int counter;
 	// Use this for initialization
 	void Start () {
-		counter = 0;
+		counter = 100;
 		Grenade = Resources.Load ("Grenade") as GameObject;
+		dir = 1f;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		counter ++;
-		if (CrossPlatformInputManager.GetButton ("Fire1") && counter > 100) {
+		float h = CrossPlatformInputManager.GetAxisRaw ("Horizontal");
+		if (h != 0) {
+			dir = h;
+		}
+			
+		if (CrossPlatformInputManager.GetButtonDown ("Jump") && counter > 100) {
 			counter = 0;
 			GameObject newGrenade = Instantiate (Grenade);
-			newGrenade.transform.position = this.transform.position + cam.transform.forward;
-			newGrenade.GetComponent<Rigidbody> ().AddForce (cam.transform.forward*throwStrength);
-		}/*else if (CrossPlatformInputManager.GetButton ("Fire2") && counter > 100) {
-			counter = 0;
-
-			GameObject newGrenade = Instantiate (Grenade);
-			newGrenade.transform.position = this.transform.position + cam.transform.forward;
+			newGrenade.transform.position = this.transform.position + Vector3.right*dir;
 			newGrenade.GetComponent<Rigidbody> ().velocity = this.GetComponent<Rigidbody> ().velocity;
-			newGrenade.GetComponent<Rigidbody> ().AddForce (cam.transform.forward*throwStrength/2);
-		}*/
+			newGrenade.GetComponent<Rigidbody> ().AddForce (Vector3.right*dir*throwStrength);
+
+		}else if (CrossPlatformInputManager.GetButton ("Fire2") && counter > 50) {
+			counter = 0;
+			GameObject newGrenade = Instantiate (Grenade);
+			newGrenade.transform.position = this.transform.position + Vector3.right*dir;
+			newGrenade.GetComponent<Rigidbody> ().velocity = this.GetComponent<Rigidbody> ().velocity;
+			newGrenade.GetComponent<Rigidbody> ().AddForce (Vector3.right*dir*throwStrength/2);
+		}
 	}
 }
